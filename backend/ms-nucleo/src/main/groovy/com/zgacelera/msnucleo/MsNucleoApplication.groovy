acaf73ca7812afe4ffaa6fe0785f4d3eaab6d106
igorpadua <igorpadua@tutanota.com>
2023-04-17 15:39:18 -0300

    feat: criado ms-nucleo

---
+++ backend/ms-nucleo/src/main/groovy/com/zgacelera/msnucleo/MsNucleoApplication.groovy
@@ -0,0 +1,13 @@
+package com.zgacelera.msnucleo
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class MsNucleoApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(MsNucleoApplication, args)
+    }
+
+}
---
+++ backend/ms-nucleo/src/test/groovy/com/zgacelera/msnucleo/MsNucleoApplicationTests.groovy
@@ -0,0 +1,13 @@
+package com.zgacelera.msnucleo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MsNucleoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
