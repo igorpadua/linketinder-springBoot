075231860821ac345cb6778d44e3cce8f9866716
igorpadua <igorpadua@tutanota.com>
2023-04-20 15:55:44 -0300

    feat: busca por id

--- backend/competencias/src/test/groovy/com/zgacelera/competencias/controller/CompetenciaControllerTest.groovy
+++ backend/competencias/src/test/groovy/com/zgacelera/competencias/controller/CompetenciaControllerTest.groovy
@@ -3,6 +3,7 @@ package com.zgacelera.competencias.controller
 import com.zgacelera.competencias.model.Competencia
 import com.zgacelera.competencias.service.CompetenciaServiceMock
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.web.server.ResponseStatusException
 import spock.lang.Specification
 
 @SpringBootTest
@@ -25,5 +26,27 @@ class CompetenciaControllerTest extends Specification {
 
         then:
         competenciaSalva != null
+    }
+
+    void "Buscar competencia por id"() {
+        given:
+        Competencia competencia = new Competencia(id: 1L)
+
+        when:
+        Competencia competenciaSalva = competenciaController.buscaPorId(competencia.id)
+
+        then:
+        competenciaSalva != null
+    }
+
+    void "Busca competencia por id inexistente"() {
+        given:
+        Competencia competencia = new Competencia(id: 2L)
+
+        when:
+        competenciaController.buscaPorId(competencia.id)
+
+        then:
+        thrown(ResponseStatusException)
     }
 }
