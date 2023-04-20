eae699e3f9d64755cafa8d779ade9aac159b303e
igorpadua <igorpadua@tutanota.com>
2023-04-20 15:23:39 -0300

    feat: init competencias

---
+++ backend/competencias/src/main/groovy/com/zgacelera/competencias/CompetenciasApplication.groovy
@@ -0,0 +1,13 @@
+package com.zgacelera.competencias
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class CompetenciasApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(CompetenciasApplication, args)
+    }
+
+}
---
+++ backend/competencias/src/test/groovy/com/zgacelera/competencias/CompetenciasApplicationTests.groovy
@@ -0,0 +1,13 @@
+package com.zgacelera.competencias
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CompetenciasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
