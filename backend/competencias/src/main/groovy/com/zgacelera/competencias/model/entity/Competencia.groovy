2613bbf9676facf8d58de3a23597cd62ca1f0287
igorpadua <igorpadua@tutanota.com>
2023-04-24 15:03:06 -0300

    feat: banco postgresql

--- backend/competencias/src/main/groovy/com/zgacelera/competencias/model/entity/Competencia.groovy
+++ backend/competencias/src/main/groovy/com/zgacelera/competencias/model/entity/Competencia.groovy
@@ -1,14 +1,18 @@
 package com.zgacelera.competencias.model.entity
 
+import groovy.transform.Canonical
 import jakarta.persistence.Entity
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
+import jakarta.persistence.Table
 
 @Entity
+@Canonical
+@Table(name = "competencias")
 class Competencia {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     Long id
     String nome
     String nivel
---
+++ backend/competencias/src/main/resources/sheme.sql
@@ -0,0 +1,5 @@
+create table competencias (
+                              id SERIAL PRIMARY KEY,
+                              nome VARCHAR(50) NOT NULL,
+                              nivel VARCHAR(50) NOT NULL
+);
