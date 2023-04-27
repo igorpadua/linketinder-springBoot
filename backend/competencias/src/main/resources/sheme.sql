7c0d78301588b5359503cd6a7463bcae57f9a047
igorpadua <igorpadua@tutanota.com>
2023-04-27 12:56:19 -0300

    feat: adicionado id pessoa e se é vaga na competencia

--- backend/competencias/src/main/resources/sheme.sql
+++ backend/competencias/src/main/resources/sheme.sql
@@ -1,5 +1,7 @@
 create table competencias (
                               id SERIAL PRIMARY KEY,
                               nome VARCHAR(50) NOT NULL,
-                              nivel VARCHAR(50) NOT NULL
+                              nivel VARCHAR(50) NOT NULL,
+                              id_usuario INTEGER NOT NULL,
+                              is_vaga BOOLEAN NOT NULL
 );
