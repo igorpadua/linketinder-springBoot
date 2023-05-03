ae00b51c786b6dc12da02c23404a6fb1f158291f
igorpadua <igorpadua@tutanota.com>
2023-05-03 09:11:51 -0300

    feat: competencias vagas

--- frontend/src/app/components/like-vaga/like-vaga.component.ts
+++ frontend/src/app/components/like-vaga/like-vaga.component.ts
@@ -3,6 +3,8 @@ import {Vaga} from "../../model/vaga.model";
 import {VagaService} from "../../service/vaga.service";
 import {CurtidaCandidatoService} from "../../service/curtida-candidato.service";
 import {Curtida} from "../../model/curtida.model";
+import {Competencia} from "../../model/competencia.model";
+import {CompetenciaService} from "../../service/competencia.service";
 
 @Component({
   selector: 'app-like-vaga',
@@ -20,25 +22,45 @@ export class LikeVagaComponent implements OnInit {
 
   idCandidato: number | undefined;
 
+  competencias: Competencia[] = []
+
   curtidaCandidato: Curtida = {
     vaga_id: 0,
     candidato_id: 0
   }
 
-  constructor(private vagaService: VagaService, private curtidaCandidatoService: CurtidaCandidatoService) {
+  constructor(private vagaService: VagaService, private curtidaCandidatoService: CurtidaCandidatoService, private competenciaService: CompetenciaService) {
   }
 
   ngOnInit(): void {
     this.vagaService.read().subscribe(vagas => {
       this.vagas = vagas;
       this.vaga = vagas[0];
+      if (this.vaga.id != null) {
+        this.competencias = this.pegaCompetencias(this.vaga.id)
+      }
     });
+  }
+
+  pegaCompetencias(id: number): Competencia[] {
+    let listaCompetencias: Competencia[] = []
+    this.competenciaService.read().subscribe(competencias => {
+      competencias.forEach(competencia => {
+        if (competencia.idPessoa === id && competencia.isVaga === true) {
+          listaCompetencias.push(competencia)
+        }
+      })
+    })
+    return listaCompetencias
   }
 
   proximo() {
     const index = this.vagas.indexOf(this.vaga);
     if (index < this.vagas.length - 1) {
       this.vaga = this.vagas[index + 1];
+      if (this.vaga.id != null) {
+        this.pegaCompetencias(this.vaga.id)
+      }
     } else {
       this.vaga = this.vagas[0];
     }
